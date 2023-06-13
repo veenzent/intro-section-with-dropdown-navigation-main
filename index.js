acb20d3d005507bc76cdb4d87a5e5dcd38f90f89
ODUME VINCENT TUOYO <odumevincent19@gmail.com>
2023-06-13 16:34:31 +0100

    Worked on media querries/responsiveness

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
+let screenWidth = window.innerWidth;
+
 function featurePopup() {
   let features = document.getElementById("fdd_menu");
   features.classList.toggle("display");
   var f_count = features.classList.length;
-  if (f_count == 4) {
+  if (f_count==4) {
     document.getElementById("feat_arr_dwn").style.display="none"
     document.getElementById("feat_arr_up").style.display="flex"
-  } else if (f_count == 3) { 
+    features.style.position = screenWidth<=768 ? "relative" : "absolute"
+  } else if (f_count==3) { 
     document.getElementById("feat_arr_dwn").style.display="flex"
     document.getElementById("feat_arr_up").style.display="none"
+    features.style.position = screenWidth<=768 ? "absolute" : "ralative"
   };
+
 }
 
 function companyPopup() {
   let company = document.getElementById("cdd_menu");
   company.classList.toggle("display");
   var c_count = company.classList.length;
-    if (c_count == 3) {
+  console.log(c_count)
+    if (c_count == 4) {
       document.getElementById("comp_arr_dwn").style.display="none"
       document.getElementById("comp_arr_up").style.display="flex"
-  } else if (c_count == 2) { 
+      company.style.position=screenWidth<=999 ? "relative" : "absolute"
+    } else if (c_count == 3) { 
       document.getElementById("comp_arr_dwn").style.display="flex"
       document.getElementById("comp_arr_up").style.display="none"
+      company.style.position=screenWidth<=999 ? "absolute" : "ralative"
   };
+}
+
+// alert(`Device width is ${screenWidth}`)
+
+function openMenu() {
+  let open = document.getElementById("mobile-nav");
+  open.classList.toggle("open")
+  open.style.display = "flex"
 }
